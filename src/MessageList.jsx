f8e4572ce04d48abcc589b0ae6bc203818aa88ce
Chandra <krsna.c@gmail.com>
2017-12-01 22:26:24 +0000

    removed dead code

--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -4,45 +4,26 @@ import Message from './Message.jsx';
 
 import Notification from './Notification.jsx';
 
-// class MessageList extends Component {
-//   render() {
-//     const messages = this.props.messages.map((message) => {
-//       if(message.type === 'sysMessage') {
-//         return <Notification
-//         key={message.id}
-//         content={message.content} />
-//       } else {
-//         return <Message
-//         key={message.id}
-//         username={message.username}
-//         content={message.content} />
-//       }
-//     });
-//     return (
-//       <main className="messages">
-//         {messages}
-//       </main>
-//     );
-//   }
-// }
-function MessageList({messages}){
-  const messageComps = messages.map((message) => {
-    if(message.type === 'sysMessage') {
-      return <Notification
-      key={message.id}
-      content={message.content} />
-    } else {
-      return <Message
-      key={message.id}
-      username={message.username}
-      content={message.content} />
-    }
-  });
-
-  return (
-    <main className="messages">
-      {messageComps}
-    </main>
-  );
+class MessageList extends Component {
+  render() {
+    const messages = this.props.messages.map((message) => {
+      if(message.type === 'sysMessage') {
+        return <Notification
+        key={message.id}
+        content={message.content} />
+      } else {
+        return <Message
+        key={message.id}
+        username={message.username}
+        content={message.content} />
+      }
+    });
+    return (
+      <main className="messages">
+        {messages}
+      </main>
+    );
+  }
 }
+
 export default MessageList;
